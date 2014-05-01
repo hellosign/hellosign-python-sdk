fb1fa07d413682613d0d9991783102d48706bc3e
Ben Dezile <ben@hellofax.com>
2014-05-01 12:20:03 -0700

    Better account tests

--- hellosign_sdk/tests/functional_tests/test_account.py
+++ hellosign_sdk/tests/functional_tests/test_account.py
@@ -2,36 +2,90 @@ from unittest import TestCase
 from hellosign_sdk.tests.test_helper import api_key
 from hellosign_sdk.hsclient import HSClient
 from hellosign_sdk.resource.account import Account
-from hellosign_sdk.utils.exception import BadRequest
-
+from hellosign_sdk.utils.exception import BadRequest, Unauthorized
+from time import time
 
 class TestAccount(TestCase):
 
     def setUp(self):
         self.client = HSClient(api_key=api_key)
 
-    def test_get_account_info(self):
-        result = self.client.get_account_info()
-        self.assertEquals(result, True)
-        self.assertEquals(isinstance(self.client.account, Account), True)
+    def test_create_account(self):
+        ''' Test creating new accounts '''
 
-        new_client = HSClient(api_key='non valid api key')
-        result = new_client.get_account_info()
-        self.assertEquals(result, False)
+        # Bad account data
+        try:
+            self.client.create_account("not valid email@example.com", "password")
+            self.fail()
+        except BadRequest:
+            pass
+        try:
+            self.client.create_account("", "password")
+            self.fail()
+        except BadRequest:
+            pass
+        try:
+            self.client.create_account("email@example.com", "")
+            self.fail()
+        except BadRequest:
+            pass
+
+        # Valid account creation
+        email = "py-sdk-test-%s@example.com" % time()
+        pwd = "somepassword"
+        try:
+            result = self.client.create_account(email, pwd)
+            self.assertEquals(isinstance(result, Account), True)
+        except:
+            self.fail()
+
+        # Already exists
+        try:
+            self.client.create_account(email, pwd)
+            self.fail()
+        except BadRequest, e:
+            self.assertTrue(e.message.find('account already exists') > 0)
+
+    def test_get_account_info(self):
+        ''' Test retrieving account information '''
+
+        # Valid account
+        result = self.client.get_account_info()
+        self.assertEquals(isinstance(result, Account), True)
+
+        # Account does not exist
+        try:
+            new_client = HSClient(api_key='non valid api key')
+            new_client.get_account_info()
+            self.fail()
+        except Unauthorized:
+            pass
 
     def test_update_account_info(self):
-        # We update nothing, but the api returns an Account object, so it is
-        # considered successful
-        result = self.client.get_account_info()
-        self.assertEquals(result, True)
-        result = self.client.update_account_info()
-        self.assertEquals(result, True)
+        ''' Test updating account info '''
 
-        self.client.account.callback_url = 'not valid url'
-        account = self.client.update_account_info()
-        self.assertEquals(account, False)
+        # We update nothing, but the api returns an Account object, so it is considered successful
+        result = self.client.get_account_info()
+        self.assertEquals(isinstance(result, Account), True)
+
+        # Invalid callback url
+        try:
+            self.client.account.callback_url = 'not valid url'
+            self.client.update_account_info()
+            self.fail()
+        except BadRequest:
+            pass
+
+        # Valid update
+        new_callback_url = 'http://www.example.com/mycallback'
+        self.client.account.callback_url = new_callback_url
+        result = self.client.update_account_info()
+        print "RESULT = %s" % result
+        self.assertEquals(isinstance(result, Account), True)
+        self.assertEquals(result.callback_url, new_callback_url)
 
     def test_account_verify(self):
+        ''' Test verifying the existence of a given account '''
         self.client.get_account_info()
         acct = self.client.account
         if acct.is_paid_hs:
@@ -39,18 +93,3 @@ class TestAccount(TestCase):
             self.assertFalse(self.client.verify_account(email='not-an-account@example.com'))
         else:
             print "WARNING: Skipping test_account_verify because it requires a paid API plan"
-
-    def test_create_account_with_invalid_info(self):
-        try:
-            self.client.create_account("not valid email@example.com",
-                                       "password")
-        except BadRequest:
-            pass
-        try:
-            self.client.create_account("", "password")
-        except BadRequest:
-            pass
-        try:
-            self.client.create_account("email@example.com", "")
-        except BadRequest:
-            pass
