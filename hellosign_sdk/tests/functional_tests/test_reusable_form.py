a64403d68afc40402a75e506029f6336f489e790
Ben Dezile <ben@hellofax.com>
2014-05-01 16:30:45 -0700

    Better reusable form tests

--- hellosign_sdk/tests/functional_tests/test_reusable_form.py
+++ hellosign_sdk/tests/functional_tests/test_reusable_form.py
@@ -4,6 +4,7 @@ from hellosign_sdk.hsclient import HSClient
 from hellosign_sdk.resource.team import Team
 from hellosign_sdk.resource.reusable_form import ReusableForm
 from hellosign_sdk.utils.exception import Forbidden, NotFound
+from time import time
 
 
 class TestReusableForm(TestCase):
@@ -11,51 +12,57 @@ class TestReusableForm(TestCase):
     def setUp(self):
         self.client = HSClient(api_key=api_key)
 
-    def test_reusable_form(self):
-        ''' Test reusable form methods '''
-        # Get reusable form list, if there's any:
-        # Add a user to our team, get the first one
-        # if no team exist, create team
-        # then add and remove a user from/to this reusableform
-        # remove user from our team
-        # destroy team
-        rfl = self.client.get_reusable_form_list()
-        self.assertTrue(isinstance(rfl, list))
-        create_team = False
-        if len(rfl) > 0:
-            self.assertTrue(isinstance(rfl[0], ReusableForm))
+    def _get_team(self):
+        ''' Get or create the current team '''
 
-            try:
-                team = self.client.get_team_info()
-            except NotFound:
-                team = self.client.create_team("Team Name")
-                self.assertTrue(isinstance(team, Team))
-                self.assertEquals(team.name, "Team Name")
-                create_team = True
-            try:
-                new_team = self.client.add_team_member("demo@example.com")
-                self.assertTrue(isinstance(new_team, Team))
-                team = new_team
-                # self.assertTrue("demo@example.com" in [account["email_address"].encode('UTF8') for account in team.accounts])
-            except Forbidden:
-                pass
-            rf = self.client.get_reusable_form(rfl[0].reusable_form_id)
-            self.assertTrue(isinstance(rf, ReusableForm))
-
-            try:
-                rf = self.client.add_user_to_reusable_form(
-                    rfl[0].reusable_form_id, None, "demo@example.com")
-                self.assertTrue(isinstance(rf, ReusableForm))
-
-                rf = self.client.remove_user_from_reusable_form(
-                    rfl[0].reusable_form_id, None, "demo@example.com")
-                self.assertTrue(isinstance(rf, ReusableForm))
-            except Forbidden:
-                pass
-            new_team = self.client.remove_team_member("demo@example.com")
+        try:
+            team = self.client.get_team_info()
+        except NotFound:
+            team = self.client.create_team("Team Name")
             self.assertTrue(isinstance(team, Team))
-            self.assertFalse("demo@example" in [account["email_address"].encode('UTF8') for account in team.accounts])
+            self.assertEquals(team.name, "Team Name")
 
-            if create_team is True:
-                result = self.client.destroy_team()
-                self.assertTrue(result)
+        if len(team.accounts) < 2:
+            try:
+                team = self.client.add_team_member("demo+%s@example.com" % time())
+                self.assertTrue(isinstance(team, Team))
+            except Forbidden:
+                pass
+
+        return team
+
+    def _get_one_reusable_form(self):
+        ''' Return one reusable form '''
+
+        reusable_forms = self.client.get_reusable_form_list()
+        self.assertTrue(isinstance(reusable_forms, list))
+        self.assertTrue(len(reusable_forms) > 0, "CREATE A TEMPLATE BEFORE RUNNING THIS TEST")
+
+        for reusable_form in reusable_forms:
+            self.assertTrue(isinstance(reusable_form, ReusableForm))
+
+        return reusable_form
+
+    def test_reusable_form_list_and_get(self):
+        ''' Test listing and retrieve reusable forms '''
+        reusable_form = self._get_one_reusable_form()
+        result = self.client.get_reusable_form(reusable_form.reusable_form_id)
+        self.assertTrue(isinstance(result, ReusableForm))
+        self.assertEquals(result.reusable_form_id, reusable_form.reusable_form_id)
+
+    def test_add_remove_reusable_form_users(self):
+        ''' Test adding and removing users from the reusable form '''
+
+        team = self._get_team()
+        reusable_form = self._get_one_reusable_form()
+        rf_id = reusable_form.reusable_form_id
+        email = team.accounts[-1]['email_address']
+
+        try:
+            r = self.client.add_user_to_reusable_form(rf_id, None, email)
+            self.assertTrue(isinstance(r, ReusableForm))
+
+            r = self.client.remove_user_from_reusable_form(rf_id, None, email)
+            self.assertTrue(isinstance(r, ReusableForm))
+        except Forbidden:
+            pass
