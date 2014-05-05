1da7bb5b609e189e7856ecd508cbee0ced31e402
Ben Dezile <ben@hellofax.com>
2014-05-05 13:27:45 -0700

    Updated setup info

--- setup.py
+++ setup.py
@@ -1,28 +1,26 @@
 from setuptools import setup
 import sys
 import os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__),
-                                'hellosign_sdk'))
 
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'hellosign_sdk'))
 
 def readme():
     with open('README.md') as f:
         return f.read()
 
 setup(name='hellosign-python-sdk',
-      version='0.3.8',
-      description="An API wrapper written in Python to interact with \
-            HelloSign's API (http://www.hellosign.com)",
+      version='1.0.0',
+      description="An API wrapper written in Python to interact with HelloSign's API (http://www.hellosign.com)",
       long_description=readme(),
       classifiers=[
-      'Development Status :: 3 - Alpha',
-      'License :: OSI Approved :: MIT License',
-      'Programming Language :: Python :: 2.7',
+        'Development Status :: 1',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
       ],
       keywords='hellosign python sdk',
-      url='https://github.com/minhdanh/hellosign-python-sdk',
-      author='Minh Danh',
-      author_email='minhdanh@siliconstraits.vn',
+      url='https://github.com/HelloFax/hellosign-python-sdk',
+      author='HelloSign',
+      author_email='apisupport@hellosign.com',
       license='MIT',
       packages=[
         'hellosign_sdk',
@@ -30,7 +28,7 @@ setup(name='hellosign-python-sdk',
         'hellosign_sdk.resource',
         ],
       install_requires=[
-      'requests'
+        'requests'
       ],
       test_suite='nose.collector',
       tests_require=['nose'],
