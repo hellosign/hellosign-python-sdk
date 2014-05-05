1087a22c51a108d4c3a9b36313c11d11c91d587f
Ben Dezile <ben@hellofax.com>
2014-05-05 14:39:55 -0700

    Minor tweak

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -59,7 +59,7 @@ copyright = u'2014, HelloSign'
 # built documents.
 #
 # The short X.Y version.
-version = '1.0.0'
+version = '1.0'
 # The full version, including alpha/beta/rc tags.
 release = '1.0.0'
 
