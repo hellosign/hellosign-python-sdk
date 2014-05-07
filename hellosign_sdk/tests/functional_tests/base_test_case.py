27e801e2c1a17f8845f05b390d7c3f55321b573e
Ben Dezile <ben@hellofax.com>
2014-05-06 18:46:51 -0700

    Show warning when running tests against prod

--- hellosign_sdk/tests/functional_tests/base_test_case.py
+++ hellosign_sdk/tests/functional_tests/base_test_case.py
@@ -5,6 +5,8 @@ from hellosign_sdk.tests import test_helper
 class BaseTestCase(TestCase):
     ''' Base for all test cases '''
 
+    warned = False
+
     def setUp(self):
         params = {
             'api_key': test_helper.api_key
@@ -15,3 +17,26 @@ class BaseTestCase(TestCase):
             pass
         self.client = HSClient(**params)
         self.client_id = test_helper.client_id
+
+        if params['env'] == 'production' and not BaseTestCase.warned:
+            BaseTestCase.warned = True
+            print "\n\n"
+            print " =================================================================="
+            print " = WARNING: We advice against running those tests against your    ="
+            print " = personal account as it performs destructive actions            ="
+            print " =================================================================="
+            while True:
+                resp = raw_input(' > Continue (type yes or hit Ctrl+C to exit)?')
+                if resp == 'yes':
+                    # Continue to tests
+                    break
+                elif resp == 'no':
+                    # Stop here
+                    import sys
+                    sys.exit()
+                else:
+                    # Ask question again
+                    pass 
+            print
+
+        print
