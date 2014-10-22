4512b49db8da6fa91b37647311297ac5aa16ef3f
Ben Dezile <ben@hellosign.com>
2014-10-22 11:31:38 -0700

    Added instructions for pre-test setup

--- hellosign_sdk/tests/test_helper.sample.py
+++ hellosign_sdk/tests/test_helper.sample.py
@@ -1,6 +1,13 @@
 import logging
 import os
 
+# NOTE: Setup instructions
+# Before running the tests, do the following
+# ===========================================
+# 1. Create an account, upgrade to Business
+# 2. Create an api app, set the client id and secret as environment variables
+# 3. Create two templates, both with a single role named 'Signer'
+
 # Logging
 logging.basicConfig(level=logging.INFO)
 
