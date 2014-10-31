77b47570f560ccb1bf98f21fe8fd7d1c3aed2d8c
Ben Dezile <ben@hellosign.com>
2014-10-31 13:02:21 -0700

    Bumped version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
         return f.read()
 
 setup(name='hellosign-python-sdk',
-      version='3.4',
+      version='3.5',
       description="A Python wrapper for the HelloSign API (http://www.hellosign.com/api)",
       long_description=readme(),
       classifiers=[
