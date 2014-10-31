ecbd9328c08bbc9f5c113387a418c3da468b46d5
Ben Dezile <ben@hellosign.com>
2014-10-31 12:58:10 -0700

    Added more test instructions

--- hellosign_sdk/tests/test_helper.sample.py
+++ hellosign_sdk/tests/test_helper.sample.py
@@ -4,7 +4,7 @@ import os
 # NOTE: Setup instructions
 # Before running the tests, do the following
 # ===========================================
-# 1. Create an account, upgrade to Business
+# 1. Create an account, upgrade to Business and add an API plan
 # 2. Create an api app, set the client id and secret as environment variables
 # 3. Create two templates, both with a single role named 'Signer'
 
