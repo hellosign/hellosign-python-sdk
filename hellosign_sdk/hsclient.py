35048f2591de850f34d500f987166ed6965df1c0
oconnor-sn <58253517+oconnor-sn@users.noreply.github.com>
2020-01-06 12:29:23 +0000

    Update hellosign_sdk/hsclient.py
    Co-Authored-By: Martin Todorov <martin.todorov@hellosign.com>

--- hellosign_sdk/hsclient.py
+++ hellosign_sdk/hsclient.py
@@ -409,7 +409,7 @@ class HSClient(object):
 
             signing_options (dict, optional): Allows the requester to specify the types allowed for creating a signature. Defaults to account settings.
 
-            attachments (list of dict):            A list of attachments, which each has the following attributes:
+            attachments (list of dict):            A list of attachments, each with the following attributes:
                 name (str):                        The name of the attachment
                 instructions (str):                The instructions for uploading the attachment
                 signer_index (int):                The index of the signer who needs to upload the attachments, see signers parameter for more details
