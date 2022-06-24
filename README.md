27a6f5a0d63d19f9832ff9c12b6b5076054e5407
Juan Treminio <juan.treminio@dropbox.com>
2022-06-24 12:43:48 -0500

    Updates version string

--- README.md
+++ README.md
@@ -57,7 +57,7 @@ this command.
 
 ### Requirements.
 
-Python 3.7
+Python >=3.7
 
 ### easy_install
 
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     author_email="apisupport@hellosign.com",
     url="",
     keywords=["OpenAPI", "OpenAPI-Generator", "HelloSign API"],
-    python_requires="3.7",
+    python_requires=">=3.7",
     install_requires=REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
